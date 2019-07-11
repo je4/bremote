f30c87798c5e9b1035a2a64d676d56a9481c1386
Jürgen Enge <juergen.enge@fhnw.ch>
2019-07-11 15:05:32 +0200

    anime added

--- controller/inittest.sh
+++ controller/inittest.sh
@@ -2,6 +2,7 @@
 
 # client01
 
+echo set https://localhost:8443/kvstore/client01/wstest.html to '{"title":"Client 01","wsAddr":"wss://localhost:443/ws"}'
 curl --header "Content-Type: application/json" \
   --request PUT \
   --data '{"title":"Client 01","wsAddr":"wss://localhost:443/ws"}' \
@@ -12,6 +13,7 @@ echo ""
 
 # client02
 
+echo set https://localhost:8443/kvstore/client02/wstest.html to '{"title":"Client 02","wsAddr":"wss://localhost:444/ws"}'
 curl --header "Content-Type: application/json" \
   --request PUT \
   --data '{"title":"Client 02","wsAddr":"wss://localhost:444/ws"}' \
@@ -20,17 +22,46 @@ curl --header "Content-Type: application/json" \
 echo ""
 
 
+# mac02
+
+echo set https://localhost:8443/kvstore/mac02/wstest.html to '{"title":"Mac 02","wsAddr":"wss://localhost:443/ws"}'
+curl --header "Content-Type: application/json" \
+  --request PUT \
+  --data '{"title":"Mac 02","wsAddr":"wss://localhost:443/ws"}' \
+  --insecure \
+  https://localhost:8443/kvstore/mac02/wstest.html
+echo ""
+
+
 # groups
 
+echo add client01 to group test
 curl --header "Content-Type: application/json" \
   --request PUT \
   --data '{"instance":"client01"}' \
   --insecure \
   https://localhost:8443/groups/test
 echo ""
+echo add client02 to group test
 curl --header "Content-Type: application/json" \
   --request PUT \
   --data '{"instance":"client02"}' \
   --insecure \
   https://localhost:8443/groups/test
+echo ""
+echo add mac02 to group test
+curl --header "Content-Type: application/json" \
+  --request PUT \
+  --data '{"instance":"mac02"}' \
+  --insecure \
+  https://localhost:8443/groups/test
+echo ""
+
+# load mac02 website
+echo navigate mac02 to https://localhost:443/controller01/templates/wstest.html
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"url": "https://localhost:443/controller01/templates/wstest.html","nextstatus":"wstest"}' \
+  --insecure \
+  https://localhost:8443/client/mac02/navigate
 echo ""
